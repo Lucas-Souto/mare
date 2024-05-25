5bcf2c4db491dee5879682a9d16711df228cb403
= <contato.lucassouto@outlook.com>
2024-05-25 11:25:01 -0300

    Construção completa das peças.

--- src/response.cpp
+++ src/response.cpp
@@ -1,7 +1,44 @@
 #include "response.hpp"
 #include "server.hpp"
+#include <string.h>
 
 static const char VAR_INDICATOR = '$';
+
+std::string getTag(int start, std::string content, int length)
+{
+	std::string result = "";
+	
+	for (int i = start; i < length; i++)
+	{
+		if (content[i] == ' ' || content[i] == '>' || content[i] == '/') break;
+		else result += content[i];
+	}
+
+	return result;
+}
+
+HTML* getHTML(const char* id, const char* filePath, std::list<HTML*> list, bool createIfNotExists)
+{
+	HTML* result = nullptr;
+
+	for (HTML* html : list)
+	{
+		if (strcmp(html->id, id) == 0)
+		{
+			result = html;
+
+			break;
+		}
+	}
+
+	if (result == nullptr && createIfNotExists)
+	{
+		result = new HTML(id, getBody(filePath));
+		list.push_back(result);
+	}
+
+	return result;
+}
 
 HTML::HTML(const char* id, std::string content)
 {
@@ -9,6 +46,7 @@ HTML::HTML(const char* id, std::string content)
 	int length = content.size();
 	bool readingVar = false;
 	std::string current = "";
+	std::string readingTag = "";
 
 	for (int i = 0; i < length; i++)
 	{
@@ -41,6 +79,49 @@ HTML::HTML(const char* id, std::string content)
 			}
 
 			readingVar = !readingVar;
+		}
+		else if (readingTag != "")
+		{
+			if (content[i] == '/' && content[i + 1] == '>' && length - 1 > i)
+			{
+				current += content[i];
+				current += content[i + 1];
+				i++;
+				readingTag = "";
+
+				this->pieces.push_back(current);
+
+				current = "";
+			}
+			else if (content[i] == '<' && content[i + 1] == '/' && length - 1 > i + readingTag.size() + 2)
+			{
+				current += content.substr(i, 3 + readingTag.size());
+				i += 2 + readingTag.size();
+				readingTag = "";
+
+				this->pieces.push_back(current);
+
+				current = "";
+			}
+			else current += content[i];
+		}
+		else if (!readingVar && content[i] == '<' && length - 1 > i && content[i + 1] != '/')
+		{
+			readingTag = getTag(i + 1, content, length);
+
+			if (readingTag != "" && getHTML(readingTag.c_str(), nullptr, Server::get()->elements, false) != nullptr)
+			{
+				this->pieces.push_back(current);
+
+				current = content.substr(i, 1 + readingTag.size());
+				i += readingTag.size();
+			}
+			else
+			{
+				current += content.substr(i, 1 + readingTag.size());
+				i += readingTag.size();
+				readingTag = "";
+			}
 		}
 		else current += content[i];
 	}
@@ -168,29 +249,6 @@ std::string getBody(const char* filePath)
 	}
 
 	return output;
-}
-
-HTML* getHTML(const char* id, const char* filePath, std::list<HTML*> list, bool createIfNotExists)
-{
-	HTML* result = nullptr;
-
-	for (HTML* page : list)
-	{
-		if (page->id == id)
-		{
-			result = page;
-
-			break;
-		}
-	}
-
-	if (result == nullptr && createIfNotExists)
-	{
-		result = new HTML(id, getBody(filePath));
-		list.push_back(result);
-	}
-
-	return result;
 }
 
 std::string render(const char* filePath, CharDict* dict)
