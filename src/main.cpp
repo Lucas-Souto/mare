d71235d27ba15f68e6be403a31501ab855cf76ef
= <contato.lucassouto@outlook.com>
2024-06-01 08:15:15 -0300

    Encerrando servidor apropriadamente.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
-#include <iostream>
+#include <csignal>
 #include "Server.hpp"
+
+Server* server;
+
+void signalHandler(int sig)
+{
+	if (sig == 2) server->Close();
+}
 
 int main()
 {
-	Server* server = new Server();
+	server = new Server();
 
+	signal(SIGINT, signalHandler);
 	server->InitLua();
+
+	printf("Pressione CTRL + C para encerrar o servidor.\n");
 	
-	while (server->IsRunning()) { }
+	while (server->IsRunning());
+
+	printf("\nEncerrando servidor...\n");
 
 	return 0;
 }
